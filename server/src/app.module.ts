68492e2b6f90879e908ca36e8f3cef8546e8ff63
Mehdi <mehdiougadiofficial@gmail.com>
2025-12-31 18:40:51 -0500

    [NEW] Mehdi: added the modules Im going to need for the project

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DiagramsModule } from './diagrams/diagrams.module';
+import { CollaborationModule } from './collaboration/collaboration.module';
+import { ValidationModule } from './validation/validation.module';
+import { ExportModule } from './export/export.module';
+import { CatalogModule } from './catalog/catalog.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('DATABASE_CONNECTION_STRING'),
+      }),
+    }),
+    DiagramsModule,
+    CollaborationModule,
+    ValidationModule,
+    ExportModule,
+    CatalogModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
